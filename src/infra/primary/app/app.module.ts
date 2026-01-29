e50a061ac7deda64ae52b15ac497f5c8b22295c8
TarekBentarzi <tarek.bentarzi@hotmail.com>
2026-01-29 14:33:33 +0100

    fix imports

--- src/infra/primary/app/app.module.ts
+++ src/infra/primary/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './infra/app/app.controller';
+import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
 
 @Module({
