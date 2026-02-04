765a1bccdb5c7e164eb1d2dabbc5bebd0ef707f9
TarekBentarzi <tarek.bentarzi@hotmail.com>
2026-02-04 17:47:34 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS for frontend connectivity
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(process.env.PORT ?? 3000);
